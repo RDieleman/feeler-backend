0d0c4a053fe72605093cdd87d3260464b4a5e7d9
Ruben <24549041+RDieleman@users.noreply.github.com>
2020-11-24 15:28:31 +0100

    Initial commit

---
+++ src/main/java/com/example/feelerbackend/FeelerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.feelerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelerBackendApplication.class, args);
+	}
+
+}
