f011424136e8e67a1a2e0db99f7a4314b9c3cc04
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 01:27:32 +0100

    Updated pom file location

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,4 @@ echo "================================="
 echo "========== Building Jar ========="
 echo "================================="
 
-docker run --rm -v $PWD/feeler-backend:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11 "$@"
+docker run --rm -v $PWD:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11 "$@"
