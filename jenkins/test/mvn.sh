61ff93cfc99cf97ac0080cd7dc16e462af33fddc
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 01:53:52 +0100

    Changed updated old path

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -6,4 +6,4 @@ echo "================================"
 
 WORKSPACE=/home/ruben/jenkins/jenkins_home/workspace/feeler-backend
 
-docker run --rm $WORKSPACE/feeler-backend:/app -v /root/.m2:/root/.m2/ -w /app maven:3.6.3-jdk11 "$@"
+docker run --rm $WORKSPACE/code:/app -v /root/.m2:/root/.m2/ -w /app maven:3.6.3-jdk11 "$@"
