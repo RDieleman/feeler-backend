3464984ce99a8c8bce1cd0056c1ec9425c0bade4
Ruben <24549041+RDieleman@users.noreply.github.com>
2021-01-17 01:45:33 +0100

    Changed pwd to workspace location

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "================================="
 echo "========== Building Jar ========="
 echo "================================="
 
-docker run --rm -v $PWD/code:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11 "$@"
+WORKSPACE=/home/ruben/jenkins/jenkins_home/workspace/feeler-backend
+
+docker run --rm -v $WORKSPACE/code:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.6.3-jdk-11 "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,6 @@ echo "================================"
 echo "======== Testing Code =========="
 echo "================================"
 
-docker run --rm $PWD/feeler-backend:/app -v /root/.m2:/root/.m2/ -w /app maven:3.6.3-jdk11 "$@"
+WORKSPACE=/home/ruben/jenkins/jenkins_home/workspace/feeler-backend
+
+docker run --rm $WORKSPACE/feeler-backend:/app -v /root/.m2:/root/.m2/ -w /app maven:3.6.3-jdk11 "$@"
